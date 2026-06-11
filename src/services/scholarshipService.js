eebde827afd453f663602bae74bda5c15eabfd36
Tatiana <tatiestra2012@gmail.com>
2026-06-10 20:35:24 -0500

    feat(HU-15): integrar consulta de becas con Supabase

--- src/services/scholarshipService.js
+++ src/services/scholarshipService.js
@@ -1,9 +1,9 @@
 import { fetchScholarships, fetchScholarshipById } from "../api/scholarshipsApi"
 
-export const getScholarships = () => {
-  return fetchScholarships()
+export const getScholarships = async () => {
+  return await fetchScholarships()
 }
 
-export const getScholarshipById = (id) => {
-  return fetchScholarshipById(id)
+export const getScholarshipById = async (id) => {
+  return await fetchScholarshipById(id)
 }
